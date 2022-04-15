a7d9c2ee0cc09c475a482d77ee99bf008db4c0c5
dns <pochti_zvezda@mail.ru>
2022-04-15 14:39:23 +0300

    testDz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
+
+
+
     public static void main(String[] args) {
-	// write your code here
+
+        List<String> names = new ArrayList<>(); //Создаем лист
+        names.add("Bob");
+        names.add("Anna");
+        names.add("Dave");   //Добавляем в него имена
+
+
+        List<String> result = new ArrayList<>(); //Создаем лист, который будет результатом
+        for (String name : names) {              //Проходимся по нашему листу с именами
+            String s = name.substring(0, 0) + name.substring(1);  //В листе с именами работаем с каждой строкой
+            result.add(s);                                        //Получившуюся строку добавляем в список result
+        }
+        Collections.sort(result);              //Сортируем наш новый лист
+        System.out.println(result);            //Выводим в консоль
+
     }
+
+
+
+
+
 }
+
